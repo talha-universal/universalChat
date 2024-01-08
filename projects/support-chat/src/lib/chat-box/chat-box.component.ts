23fd86a1c54801bfe9068a2cc152b90488e16748
talha-universal <m.talhach@universalplus.technology>
2024-01-08 19:26:35 +0400

    config and scroll issue

--- projects/support-chat/src/lib/chat-box/chat-box.component.ts
+++ projects/support-chat/src/lib/chat-box/chat-box.component.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { CONFIG } from '../../Config';
 import { NetworkService } from '../Serives/network.service';
 import { WebsocketService } from '../Serives/websocket.service';
-
-@Component({
+import { DeviceDetectorService } from 'ngx-device-detector';
+declare var $: any; @Component({
   selector: 'lib-chat-box',
   standalone: true,
   imports: [FormsModule, NgIf, NgFor, NgClass, DatePipe],
@@ -30,14 +30,31 @@ export class ChatBoxComponent implements OnDestroy {
   // 'ws://185.182.194.244:8080'
   // private SocketBaseUrl = CONFIG.socketurl == '' ? 'wss://' + window.location.host + '/universecasino' : 'ws://185.182.194.244:8080';
   private SocketBaseUrl = 'wss://buzzmehi.com/socketChat/';
-  loginData: any ='';
+  loginData: any = '';
   sendMessageObj: any;
-  constructor(private backendService: NetworkService, private websocketService: WebsocketService) {
+  isDesktop: boolean;
+  isMobile: boolean;
+  isKeyboardMbl: boolean = true;
+  isMobileInfo: string;
+  opnKeybord: boolean = false;
+  isSendButtonClick: boolean = false;
+  constructor(private backendService: NetworkService, private websocketService: WebsocketService,
+    private devicedetector: DeviceDetectorService, private el: ElementRef) {
     this.getMessageFromSocket();
     this.LoginGuestUser();
+    this.isDesktop = this.devicedetector.isDesktop();
+    // Check if the current device is a mobile
+
+    this.isMobile = this.devicedetector.isMobile();
+    this.isDesktop = this.devicedetector.isDesktop();
+    this.isMobileInfo = this.devicedetector.os;
+
+
   }
   ngOnDestroy(): void {
     this.websocketService.closeSocket();
+    this.messages = [];
+    document.body.style.overflowY = 'auto';
   }
 
   onInputChange() {
@@ -67,8 +84,8 @@ export class ChatBoxComponent implements OnDestroy {
 
 
 
-  LoginGuestUser(userLoginObj?:any){
-    this.backendService.recordsFromLocalStorage(CONFIG.userLogin,CONFIG.userLoginTime , userLoginObj).subscribe((data: any) => {
+  LoginGuestUser(userLoginObj?: any) {
+    this.backendService.recordsFromLocalStorage(CONFIG.userLogin, CONFIG.userLoginTime, userLoginObj).subscribe((data: any) => {
       if (data.status == 'success') {
         this.guestUserLogin = true
         this.userDetails = data?.data;
@@ -91,7 +108,7 @@ export class ChatBoxComponent implements OnDestroy {
           }
         );
       }
-      else{
+      else {
         this.guestUserLogin = false
       }
 
@@ -99,28 +116,35 @@ export class ChatBoxComponent implements OnDestroy {
 
   }
 
-obj:any =
+  obj: any =
     {
 
       sender: {
-          name: "as_7",
-          email: "as_7@xyz.com",
-          domain: "xyz.com",
+        name: "as_7",
+        email: "as_7@xyz.com",
+        domain: "xyz.com",
+        _id: "1223",
       },
-      receiver:{},
+      receiver: {},
       attachments: [],
       message: "",
       messageId: "web_1704473158873",
       type: "message",
-      myMessage:false,
+      myMessage: false,
       forward: false,
       delivered: false,
       deliveredToSender: false,
       sentAt: "2024-01-05T16:45:58.873Z",
-  }
-  
+    }
+
 
   sendMessage() {
+    this.isSendButtonClick = true;
+    const chatBox = document.getElementById('chatMessage')
+    chatBox?.focus();
+    const formatDate = (date: Date): string => {
+      return date.toISOString();
+    };
     if (this.messageText.trim() !== '') {
       const current = new Date();
       this.sendMessageObj = {
@@ -130,23 +154,49 @@ obj:any =
         sentAt: new Date(),
         messageId: "web_" + current.getTime()
       }
-
-    
-      this.obj.messageId ="web_" + current.getTime();
-      this.obj.message =this.messageText;
-      this.obj.sentAt =current;
-      this.obj.myMessage = true
-      this.messages.push(this.obj);
+  
+      const newMessage = {
+        ...this.obj,  // Copy properties from the original object if needed
+        messageId: this.sendMessageObj.messageId,
+        message: this.messageText,
+        sentAt: formatDate(current),
+        myMessage: true
+      };
+      
+      this.messages.push(newMessage);
+      this.messages = this.messages.sort((a: any, b: any) => a.sentAt.localeCompare(b.sentAt));
 
       this.websocketService.send(this.sendMessageObj);
       this.messageText = '';
       // this.isSendButtonVisible = false
-      const chatBox = document.getElementById('chatMessage')
-      chatBox?.focus();
+
     }
     else {
       return
     }
+  }
+
+  binarySearch(messages: any, targetMessageId: any) {
+    let left = 0;
+    let right = messages.length - 1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (messages[mid].messageId === targetMessageId) {
+        // Found the targetMessageId
+        return mid; // Return the index of the matching object
+      }
+
+      if (messages[mid].messageId < targetMessageId) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    // If targetMessageId is not found in any object
+    return -1;
   }
 
   getMessageFromSocket() {
@@ -158,23 +208,97 @@ obj:any =
         if (socketData?.type == "message") {
 
 
-          const indexToUpdate = this.messages.findIndex((msg: any) => msg.messageId === socketData.messageId);
+          const index = this.binarySearch(this.messages, socketData.messageId);
 
-          // If the message with the same messageId is found, update it
-          if (indexToUpdate !== -1) {
+          if (index !== -1) {
+            // Do something with the found object, e.g., update it
             this.showAnimation = false;
-            this.messages[indexToUpdate] = socketData;
+            this.messages[index] = socketData;
           } else {
+            // If the message with the same messageId is not found, add it to the array
             this.showAnimation = true;
             this.messages.push(socketData);
+
+            // Assuming the messages array remains sorted, if not, you may need to sort it.
+            this.messages = this.messages.sort((a: any, b: any) => a.sentAt.localeCompare(b.sentAt));
           }
 
+
+          // const indexToUpdate = this.messages.findIndex((msg: any) => msg.messageId === socketData.messageId);
+
+          // // If the message with the same messageId is found, update it
+          // if (indexToUpdate !== -1) {
+          //   this.showAnimation = false;
+          //   this.messages[indexToUpdate] = socketData;
+          // } else {
+          //   this.showAnimation = true;
+          //   this.messages.push(socketData);
+          //   this.messages = this.messages.sort((a: any, b: any) => a.sentAt.localeCompare(b.sentAt));
+
+          // }
 
 
         }
       }
 
     })
+  }
+
+  onfocusText() {
+    this.isKeyboardMbl = false;
+    debugger
+    if (!this.opnKeybord && this.isMobile) {
+      this.setMarketScrollHeight();
+    }
+  }
+
+  setMarketScrollHeight() {
+    this.opnKeybord = true
+    const checkBoxElement = document.getElementById('checkBox');
+    const windowHeight = window.innerHeight;
+    // Adjust the percentage or calculation based on your specific needs.
+
+    if (this.isMobile) {
+      document.body.style.overflowY = 'hidden';
+    }
+    let targetHeight;
+    if (windowHeight <= 720) {
+      //  targetHeight = Math.floor(windowHeight * 0.52); //done
+      targetHeight = Math.floor(windowHeight * 0.61);
+    }
+    else if (this.isMobileInfo == 'iOS') {
+      targetHeight = Math.floor(windowHeight * 0.625);
+    }
+    else {
+      targetHeight = Math.floor(windowHeight * 0.62);
+    }
+
+    if (checkBoxElement) {
+      checkBoxElement.style.height = `${targetHeight}px`;
+      // marketScrollElement.style.marginTop = `${margintop}px`;
+    }
+  }
+
+
+  onBlurText(event: FocusEvent) {
+    setTimeout(() => {
+      const clickedElement = event.relatedTarget as HTMLElement;
+
+      // Check if the click was on the "Send" button
+      if (this.isSendButtonClick) {
+        this.isSendButtonClick = false; // Reset the flag
+        return;
+        // Perform specific actions for "Send" button click during blur
+      } else {
+        this.isKeyboardMbl = true;
+        const checkBoxElement = document.getElementById('checkBox');
+        if (checkBoxElement && this.isMobile) {
+          this.opnKeybord = false;
+          checkBoxElement.style.height = `100%`;
+        }
+      }
+    }, 300);
+
   }
 
   @HostListener('document:click', ['$event'])
@@ -188,4 +312,5 @@ obj:any =
       // do something...
     }
   }
+  // 
 }
--- projects/support-chat/src/public-api.ts
+++ projects/support-chat/src/public-api.ts
@@ -5,3 +5,4 @@
 // export * from './lib/support-chat.service';
 export * from './lib/indexService';
 export * from './lib/index';
+export * from 'ngx-device-detector';
